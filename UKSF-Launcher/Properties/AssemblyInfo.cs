5fdeeb9d7e4cf8942fbe19e80027e15f6abd1418
Tim Beswick <contact.tim.here@gmail.com>
2017-09-24 23:06:32 +0100

    Updated version

--- UKSF-Launcher/Properties/AssemblyInfo.cs
+++ UKSF-Launcher/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.0.25")]
-[assembly: AssemblyVersion("0.0.0.25")]
-[assembly: AssemblyFileVersion("0.0.0.25")]
+// [assembly: AssemblyVersion("0.0.0.27")]
+[assembly: AssemblyVersion("0.0.0.27")]
+[assembly: AssemblyFileVersion("0.0.0.27")]
