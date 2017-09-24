1d57fe81a8c477da1f045abff99b77d1ace0adce
Tim Beswick <contact.tim.here@gmail.com>
2017-09-24 21:51:14 +0100

    Should fix?

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -6,14 +6,12 @@ using System.Linq;
 namespace Updater {
     class Program {
         static void Main(string[] args) {
-            string update = Path.Combine(Environment.CurrentDirectory);
-            Console.WriteLine(update);
+            string update = Path.Combine(Environment.CurrentDirectory, "update");
 
             string[] oldFiles = Directory.GetFiles(Directory.GetParent(update).FullName);
             foreach (string file in oldFiles) {
                 if (!file.Contains("Updater.exe") && !file.Contains("update")) {
-                    //File.Delete(file);
-                    File.Move(file, file + "OLD");
+                    File.Delete(file);
                 }
             }
 
@@ -26,10 +24,7 @@ namespace Updater {
             launcher.StartInfo.FileName = Path.Combine(Directory.GetParent(update).FullName, "UKSF-Launcher.exe");
             launcher.StartInfo.Arguments = "-u";
             launcher.Start();
-            //Environment.Exit(0);
-            while(true) {
-
-            }
+            Environment.Exit(0);
         }
     }
 }
