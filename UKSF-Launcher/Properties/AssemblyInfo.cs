a0b49dbc4371cd956a421216ea7e666ddd155ab6
Tim Beswick <contact.tim.here@gmail.com>
2017-10-03 22:31:05 +0100

    Release version: 1.0.0.201

--- UKSF-Launcher/Properties/AssemblyInfo.cs
+++ UKSF-Launcher/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.161")]
+[assembly: AssemblyVersion("1.0.0.201")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
