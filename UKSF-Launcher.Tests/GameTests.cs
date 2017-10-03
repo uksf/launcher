fe9a4c1340a7ab5ee7fe7e2fe73803db4a4821b9
Tim Beswick <contact.tim.here@gmail.com>
2017-10-04 00:11:08 +0100

    Disabled appveyor automatic tests. Handled by script.

--- UKSF-Launcher.Tests/GameTests.cs
+++ UKSF-Launcher.Tests/GameTests.cs
@@ -1,11 +1,8 @@
 ﻿using NUnit.Framework;
-using UKSF_Launcher.Game;
-using UKSF_Launcher.Utility;
 
 namespace UKSF_Launcher.Tests {
     public class GameTests {
         [Test]
-        public void StartupString() {
-        }
+        public void StartupString() { }
     }
 }
