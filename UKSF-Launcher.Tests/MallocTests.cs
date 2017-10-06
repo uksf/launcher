c1c735b0c214a210b93cf6795d5565d0b0f5330d
Tim Beswick <contact.tim.here@gmail.com>
2017-10-06 04:01:03 +0100

    1

--- UKSF-Launcher.Tests/MallocTests.cs
+++ UKSF-Launcher.Tests/MallocTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
 using UKSF_Launcher.Game;
@@ -16,6 +15,7 @@ namespace UKSF_Launcher.Tests {
         [Test]
         public void MallocFind() {
             string installation = GameHandler.GetGameInstallation();
+            if (string.IsNullOrEmpty(installation)) return;
             installation = Path.GetDirectoryName(installation);
             if (string.IsNullOrEmpty(installation)) return;
             List<MallocHandler.Malloc> mallocs = MallocHandler.GetMalloc(Path.Combine(installation, "Dll"));
