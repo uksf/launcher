843219fd7e79a8651801b94bbaf473f61e09d578
Tim Beswick <contact.tim.here@gmail.com>
2017-10-27 16:58:29 +0100

    Added releaser

---
+++ Releaser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Releaser {
+    internal class Program {
+        private static void Main() {
+            Directory.SetCurrentDirectory(Path.Combine(Environment.CurrentDirectory, ".."));
+            Process.Start("git", @"checkout release")?.WaitForExit();
+            Process.Start("git", @"merge master")?.WaitForExit();
+            Process.Start("git", @"commit -am ""Release build""")?.WaitForExit();
+            Process.Start("git", @"push")?.WaitForExit();
+        }
+    }
+}
