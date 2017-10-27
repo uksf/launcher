f1cc853249f8114fe4592cf754570a631d2e46db
Tim Beswick <contact.tim.here@gmail.com>
2017-10-27 17:02:48 +0100

    Release version: 1.0.0.272

--- Releaser/Properties/AssemblyInfo.cs
+++ Releaser/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.265")]
+[assembly: AssemblyVersion("1.0.0.272")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
