1f435ca92788177dc48ea1b6fa12d2c704975d0d
Tim Beswick <contact.tim.here@gmail.com>
2017-10-31 09:37:35 +0000

    Cleaned up pbo file

--- Patching/Pbo/PboFile.cs
+++ Patching/Pbo/PboFile.cs
@@ -4,17 +4,13 @@ using System.Text;
 namespace Patching.Pbo {
     internal class PboFile {
         private readonly byte[] _fileName;
-        private ulong _packingMethod;
-        private ulong _reserved;
-        private int _size;
-        private ulong _timestamp;
 
         public PboFile(FileStream stream) {
             _fileName = Utility.ReadString(stream);
-            _packingMethod = Utility.ReadULong(stream);
-            _size = (int) Utility.ReadULong(stream);
-            _reserved = Utility.ReadULong(stream);
-            _timestamp = Utility.ReadULong(stream);
+            Utility.ReadULong(stream);
+            Utility.ReadULong(stream);
+            Utility.ReadULong(stream);
+            Utility.ReadULong(stream);
             Datasize = Utility.ReadULong(stream);
             Size = (int) Datasize;
         }
