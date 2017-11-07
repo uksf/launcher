599f2d88b75399c46ed09a56713f90014786edc9
Tim Beswick <contact.tim.here@gmail.com>
2017-11-07 17:05:03 +0000

    Added wix installer

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -7,7 +7,7 @@ using System.Threading;
 namespace Updater {
     internal static class Program {
         private static void Main() {
-            Process[] processes = Process.GetProcessesByName("UKSF-Launcher");
+            Process[] processes = Process.GetProcessesByName("Launcher");
             while (processes.Length > 0) {
                 foreach (Process process in processes) {
                     process.WaitForExit(500);
@@ -15,13 +15,14 @@ namespace Updater {
                         process.Kill();
                     }
                 }
-                processes = Process.GetProcessesByName("UKSF-Launcher");
+                processes = Process.GetProcessesByName("Launcher");
             }
             Thread.Sleep(500);
             
-            string launcher = Path.Combine(Environment.CurrentDirectory, "UKSF-Launcher.exe");
+            string launcher = Path.Combine(Environment.CurrentDirectory, "Launcher.exe");
             string patching = Path.Combine(Environment.CurrentDirectory, "Patching.dll");
             string network = Path.Combine(Environment.CurrentDirectory, "Network.dll");
+            string fastrsync = Path.Combine(Environment.CurrentDirectory, "FastRsync.dll");
             if (File.Exists(launcher)) {
                 File.SetAttributes(launcher, FileAttributes.Normal);
                 File.Delete(launcher);
@@ -34,13 +35,18 @@ namespace Updater {
                 File.SetAttributes(network, FileAttributes.Normal);
                 File.Delete(network);
             }
+            if (File.Exists(fastrsync)) {
+                File.SetAttributes(fastrsync, FileAttributes.Normal);
+                File.Delete(fastrsync);
+            }
 
-            new WebClient().DownloadFile("http://www.uk-sf.com/launcher/release/UKSF-Launcher.exe", launcher);
+            new WebClient().DownloadFile("http://www.uk-sf.com/launcher/release/Launcher.exe", launcher);
             new WebClient().DownloadFile("http://www.uk-sf.com/launcher/release/Patching.dll", patching);
             new WebClient().DownloadFile("http://www.uk-sf.com/launcher/release/Network.dll", network);
+            new WebClient().DownloadFile("http://www.uk-sf.com/launcher/release/FastRsync.dll", fastrsync);
 
             Process launcherProcess = new Process {
-                StartInfo = {UseShellExecute = false, FileName = Path.Combine(Environment.CurrentDirectory, "UKSF-Launcher.exe"), Arguments = "-u"}
+                StartInfo = {UseShellExecute = false, FileName = Path.Combine(Environment.CurrentDirectory, "Launcher.exe"), Arguments = "-u"}
             };
             launcherProcess.Start();
             Environment.Exit(0);
