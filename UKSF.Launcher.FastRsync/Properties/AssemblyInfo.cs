33cebbc0760b1089c01fe6205d349262fa1c49db
uksfelcom <uksfelcom@gmail.com>
2019-03-18 23:26:59 +0000

    1.0.0.797

--- UKSF.Launcher.FastRsync/Properties/AssemblyInfo.cs
+++ UKSF.Launcher.FastRsync/Properties/AssemblyInfo.cs
@@ -12,8 +12,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.796")]
-[assembly: AssemblyFileVersion("1.2.796")]
+[assembly: AssemblyVersion("1.0.0.797")]
+[assembly: AssemblyFileVersion("1.2.797")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
